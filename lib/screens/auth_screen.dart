20c7f04fa0d44f561214bbe877c4fcfa6be257d4
MohamedAbdElsatar <mohamedabdelsatar2018@gmail.com>
2021-07-07 17:29:45 -0700

    Authenticated form

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,36 +1,18 @@
-import 'package:chat_app/widgets/buttons_sign.dart';
 import 'package:chat_app/widgets/form_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class AuthScreen extends StatelessWidget {
+  void submitFunction(
+      String email, String username, String password, bool isLogIn) {
+    print(email);
+    print(username);
+    print(password);
+    print(isLogIn);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.purpleAccent,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 15),
-        child: SafeArea(
-          minimum: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-          child: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 70,
-                    backgroundImage: AssetImage('assets/images/avataruser.jpg'),
-                  ),
-                 FormInput(),
-                  SizedBox(height: 30),
-                 Buttons()
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return Scaffold(body: FormInput(submitFunction));
   }
 }
--- lib/widgets/form_input.dart
+++ lib/widgets/form_input.dart
@@ -1,52 +1,224 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-class FormInput extends StatelessWidget {
+class FormInput extends StatefulWidget {
+  final Function submitFunction;
+  FormInput(
+      this.submitFunction(
+    String email,
+    String username,
+    String password,
+    bool isLogIn,
+  ));
+  @override
+  _FormInputState createState() => _FormInputState();
+}
+
+class _FormInputState extends State<FormInput> {
+  final _keyForm = GlobalKey<FormState>();
+  bool _haveAccount = true;
+  bool _isVisable = true;
+  var username = "";
+  var password = "";
+  var email = "";
+  final elevationBtnStyle = ElevatedButton.styleFrom(
+    elevation: 5,
+    padding: EdgeInsets.symmetric(vertical: 12),
+    onPrimary: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20),
+    ),
+  );
+  final outlineBtnStyle = ElevatedButton.styleFrom(
+    elevation: 5,
+    padding: EdgeInsets.symmetric(vertical: 12),
+    onPrimary: Colors.red,
+    primary: Colors.white,
+    shape: RoundedRectangleBorder(
+      side: BorderSide(color: Colors.red),
+      borderRadius: BorderRadius.circular(20),
+    ),
+  );
+  final neumorphocStyle = NeumorphicStyle(
+      color: Colors.white, boxShape: NeumorphicBoxShape.stadium());
+  void _onSave() {
+    final valid = _keyForm.currentState!.validate();
+    if (!valid) {
+      print("U R Not Authentecated");
+      return;
+    }
+    _keyForm.currentState!.save();
+    widget.submitFunction(username, email, password, _haveAccount);
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-      children: [
-        Neumorphic(
-          style: NeumorphicStyle(
-              color: Colors.white, boxShape: NeumorphicBoxShape.stadium()),
-          margin: EdgeInsets.only(top: 20),
-          child: TextFormField(
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: "username",
-              contentPadding: EdgeInsets.symmetric(horizontal: 15),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: SafeArea(
+        // minimum: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('assets/images/avataruser.jpg'),
+                ),
+                Column(
+                  children: [
+                    //input form
+                    Form(
+                      key: _keyForm,
+                      child: Column(
+                        children: [
+                          if (_haveAccount)
+                            Neumorphic(
+                              style: neumorphocStyle,
+                              margin: EdgeInsets.only(top: 20),
+                              padding: EdgeInsets.only(left: 10, bottom: 5),
+                              child: TextFormField(
+                                key: ValueKey('username'),
+                                onSaved: (value) {
+                                  username = value!;
+                                },
+                                validator: (value) {
+                                  if (value!.isEmpty == true ||
+                                      value.length <= 4) {
+                                    return "Username must be more than 4 charcter!";
+                                  }
+
+                                  return null;
+                                },
+                                textInputAction: TextInputAction.next,
+                                decoration: InputDecoration(
+                                  icon: Icon(Icons.person_outline),
+                                  border: InputBorder.none,
+                                  hintText: "Username",
+                                ),
+                              ),
+                            ),
+                          Neumorphic(
+                            style: neumorphocStyle,
+                            margin: EdgeInsets.only(top: 20),
+                            padding: EdgeInsets.only(left: 10, bottom: 5),
+                            child: TextFormField(
+                              key: ValueKey('email'),
+                              onSaved: (value) {
+                                email = value!;
+                              },
+                              validator: (value) {
+                                if (value!.isEmpty ||
+                                    !value.contains('@') ||
+                                    !value.contains(".")) {
+                                  return "Please,Enter valid email address!";
+                                }
+
+                                return null;
+                              },
+                              textInputAction: TextInputAction.next,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.email_outlined),
+                                border: InputBorder.none,
+                                hintText: "Email",
+                              ),
+                            ),
+                          ),
+                          Neumorphic(
+                            style: neumorphocStyle,
+                            margin: EdgeInsets.only(top: 20),
+                            padding: EdgeInsets.only(left: 10, bottom: 5),
+                            child: TextFormField(
+                              key: ValueKey('password'),
+                              onSaved: (value) {
+                                password = value!;
+                              },
+                              validator: (value) {
+                                if (value!.isEmpty || value.length <= 5) {
+                                  return "Password must be more than 5 charctre";
+                                }
+                                return null;
+                              },
+                              textInputAction: TextInputAction.done,
+                              obscureText: _isVisable,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.lock_open),
+                                suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _isVisable = !_isVisable;
+                                      });
+                                    },
+                                    icon: _isVisable
+                                        ? Icon(Icons.visibility_off)
+                                        : Icon(Icons.visibility)),
+                                border: InputBorder.none,
+                                hintText: "Password",
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    //signUp & logIn buttons
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: _onSave,
+                                child: Text(
+                                  _haveAccount ? "SignUp" : "logIn",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                style: _haveAccount
+                                    ? elevationBtnStyle
+                                    : outlineBtnStyle,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _haveAccount = !_haveAccount;
+                                  });
+                                },
+                                child: Text(
+                                  _haveAccount
+                                      ? "Alrady have an account"
+                                      : "Create new account",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                style: _haveAccount
+                                    ? outlineBtnStyle
+                                    : elevationBtnStyle,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
         ),
-        Neumorphic(
-          style: NeumorphicStyle(
-              color: Colors.white, boxShape: NeumorphicBoxShape.stadium()),
-          margin: EdgeInsets.only(top: 20),
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: "email",
-              contentPadding: EdgeInsets.symmetric(horizontal: 15),
-            ),
-          ),
-        ),
-        Neumorphic(
-          style: NeumorphicStyle(
-              color: Colors.white, boxShape: NeumorphicBoxShape.stadium()),
-          margin: EdgeInsets.only(top: 20),
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            obscureText: false,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: "password",
-              contentPadding: EdgeInsets.symmetric(horizontal: 15),
-            ),
-          ),
-        )
-      ],
-    ));
+      ),
+    );
   }
 }
